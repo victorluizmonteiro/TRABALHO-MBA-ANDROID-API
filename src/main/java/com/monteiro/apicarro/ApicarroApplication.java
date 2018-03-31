c2c2fb21a6729ad9df1d93b805b64761eb2bac42
Victor <victorluiz.monteiro@gmail.com>
2018-03-31 14:41:05 -0300

    initial commit

---
+++ src/main/java/com/monteiro/apicarro/ApicarroApplication.java
@@ -0,0 +1,12 @@
+package com.monteiro.apicarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicarroApplication.class, args);
+	}
+}
