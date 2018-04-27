77ebc364481128e6b4780bc5a95bc27e1aa091da
Victor <victorluiz.monteiro@gmail.com>
2018-04-27 00:00:20 -0300

    alterando API

--- src/main/java/com/monteiro/apicarro/controller/CarroController.java
+++ src/main/java/com/monteiro/apicarro/controller/CarroController.java
@@ -18,53 +18,44 @@ public class CarroController {
 
 
     @PostMapping
-    public ResponseEntity<Carro> cadastrar(@RequestBody Carro carro) {
+    public Carro cadastrar(@RequestBody Carro carro) {
         try {
             carroService.salvar(carro);
-            return ResponseEntity.status(HttpStatus.CREATED).body(carro);
+            return carro;
         } catch (Exception e) {
-            return ResponseEntity.status(400).body(null);
+            return null;
         }
     }
 
     @GetMapping("/placa/{placa}")
-    public ResponseEntity<Optional<Carro>> buscarPelaPlaca(@PathVariable("placa") String placa) {
+    public Carro buscarPelaPlaca(@PathVariable("placa") String placa) {
         Optional<Carro> carro = carroService.buscarPelaPlaca(placa);
 
 
         if (!carro.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+            return null;
         }
 
-        return ResponseEntity.ok().body(carro);
+        return carro.get();
 
 
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Carro>> buscarPeloId(@PathVariable("id") String id) {
-        Optional<Carro> carro = carroService.buscarPorId(id);
 
-        if (!carro.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-        }
-        return ResponseEntity.ok().body(carro);
-
-    }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Carro> deletar(@PathVariable("id") String id) {
+    public Carro deletar(@PathVariable("id") String id) {
         Optional<Carro> carro = carroService.buscarPorId(id);
         try {
             carroService.remover(carro.get());
-            return ResponseEntity.ok(carro.get());
+            return carro.get();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
+            return null;
         }
     }
 
     @GetMapping
-    public ResponseEntity<List<Carro>> buscarTodos() {
-        return ResponseEntity.ok(carroService.listarTodos());
+    public List<Carro> buscarTodos() {
+        return carroService.listarTodos();
     }
 }
